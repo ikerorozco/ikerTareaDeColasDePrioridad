e2f29bd18228a22c81c2f8937b0516410e5967c7
ikerorozco <99369597+ikerorozco@users.noreply.github.com>
2022-11-22 00:47:03 -0600

    Update Lista.java

--- src/ikertareacolasdeprioridad/Lista.java
+++ src/ikertareacolasdeprioridad/Lista.java
@@ -8,6 +8,66 @@ package ikertareacolasdeprioridad;
  *
  * @author ikero
  */
-public class Lista {
+public class Lista <T> {
     
+    Nodo UltimoValor;
+    int tamanio;
+    String Lista = "";
+
+    public Lista() {
+        UltimoValor = null;
+        tamanio=0;
+    }
+
+    
+    public boolean isEmpty(){
+    
+        return UltimoValor==null;
+    
+    }
+    
+    public int length(){
+    
+        return tamanio;
+    
+    } 
+    
+    public T pop() {
+    
+        T aux = (T) UltimoValor;
+        UltimoValor=UltimoValor.Siguiente;
+        tamanio--;
+        return aux;
+    
+    }
+    
+    public T peek(){
+    
+        return (T) UltimoValor.valor;
+    
+    } 
+    
+    public void push( Nodo<T> nodo ){
+    
+        Nodo<T> nuevoNodo = new Nodo(nodo);
+        nuevoNodo.Siguiente = UltimoValor;
+        UltimoValor=nuevoNodo;
+        tamanio++;
+    
+    } 
+
+    @Override
+    public String toString() {
+        Nodo recorrido=UltimoValor;
+        while(recorrido !=null){
+        
+            Lista+=recorrido.valor+"\n";
+            recorrido=recorrido.Siguiente;
+        
+        }
+    
+        return Lista;
+    }
+  
 }
+
