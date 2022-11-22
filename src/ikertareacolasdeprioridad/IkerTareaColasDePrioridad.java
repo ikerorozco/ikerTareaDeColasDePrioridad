9d817c8a185d7e2661c9cf0199192b6eef547174
ikerorozco <99369597+ikerorozco@users.noreply.github.com>
2022-11-22 00:49:15 -0600

    Update IkerTareaColasDePrioridad.java

--- src/ikertareacolasdeprioridad/IkerTareaColasDePrioridad.java
+++ src/ikertareacolasdeprioridad/IkerTareaColasDePrioridad.java
@@ -14,7 +14,37 @@ public class IkerTareaColasDePrioridad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        BoundredPriorityQueue colaPrior = new BoundredPriorityQueue(5);
+        
+        System.out.println("La lista esta vacia? "+colaPrior.is_empty());
+        
+        colaPrior.enqueue(0,"niÃ±os");
+        
+        colaPrior.enqueue(0,"niÃ±as");
+        
+        colaPrior.enqueue(1,"mujeres");
+        
+        colaPrior.enqueue(1,"personas mayores");
+        
+        colaPrior.enqueue(2,"personas jovenes");
+        
+        colaPrior.enqueue(3,"hombres");
+        
+        colaPrior.enqueue(4,"personal de trabajo");
+        
+        colaPrior.enqueue(4,"capitan");
+        
+        System.out.println("La lista esta vacia? "+colaPrior.is_empty());
+        
+        System.out.println("Cual es el tamaÃ±o de la cola de prioridad? "+colaPrior.length());
+        
+        colaPrior.dequeue();
+        
+        System.out.println("Cual es el tamaÃ±o de la cola de prioridad? "+colaPrior.length());
+        
+        System.out.println("La forma de la lista es: "+colaPrior.toString());
+        
     }
     
 }
